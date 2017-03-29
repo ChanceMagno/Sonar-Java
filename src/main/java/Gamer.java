a21cbddc04ef2929337bbd1c90bfb028da2f582d
Chris Carr and Chance Magno <chris@>
2017-03-29 11:33:52 -0700

    complete Gamer class and tests

--- src/main/java/Gamer.java
+++ src/main/java/Gamer.java
@@ -1,9 +1,56 @@
 import org.sql2o.*;
+import java.util.List;
 
 public class Gamer {
+  private String name;
+  private int id;
 
-  public Gamer() {
-
+  public Gamer(String name) {
+    this.name = name;
   }
 
+  public String getName(){
+    return name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Gamer> all(){
+    try(Connection con= DB.sql2o.open()){
+      String sql = "SELECT * FROM gamers";
+      return con.createQuery(sql).executeAndFetch(Gamer.class);
+    }
+  }
+
+  public void save(){
+    try(Connection con= DB.sql2o.open()){
+      String sql = "INSERT INTO gamers (name)  VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherGamer){
+  if (!(otherGamer instanceof Gamer)) {
+    return false;
+  } else {
+    Gamer newGamer = (Gamer) otherGamer;
+    return this.getName().equals(newGamer.getName()) && this.getId() == newGamer.getId();
+    }
+  }
+
+  public static Gamer find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM gamers WHERE id = :id;";
+      Gamer gamer = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Gamer.class);
+      return gamer;
+    }
+  }
 }
--- src/test/java/GamerTest.java
+++ src/test/java/GamerTest.java
@@ -9,7 +9,46 @@ public class GamerTest {
 
   @Test
   public void gameInstantiatesCorrectly_true() {
-    Gamer test = new Gamer();
+    Gamer test = new Gamer("john");
     assertEquals(true, test instanceof Gamer);
+  }
+
+  @Test
+  public void getName_name() {
+    Gamer test = new Gamer("john");
+    assertEquals("john", test.getName());
+  }
+
+  @Test
+  public void getId_true() {
+    Gamer test = new Gamer("john");
+    test.save();
+    assertTrue(test.getId()>0);
+  }
+
+  @Test
+  public void all_getAllGamers_true() {
+    Gamer test1 = new Gamer("john");
+    test1.save();
+    Gamer test2 = new Gamer("bill");
+    test2.save();
+    assertTrue(test1.equals(Gamer.all().get(0)));
+    assertTrue(test2.equals(Gamer.all().get(1)));
+  }
+
+  @Test
+  public void save_returnsTrueIfNamesAreTheSame_true() {
+    Gamer test = new Gamer("john");
+    test.save();
+    assertTrue(Gamer.all().get(0).equals(test));
+  }
+
+  @Test
+  public void find_returnsGamerWithSameId_test2() {
+    Gamer test1 = new Gamer("john");
+    test1.save();
+    Gamer test2 = new Gamer("bill");
+    test2.save();
+    assertEquals(Gamer.find(test2.getId()), test2);
   }
 }
